65005198e9330755a18d277f4a76b496baa68dac
yyo <cheikmm@gmail.com>
2020-12-07 12:53:16 +0100

    premier comit du jour1 git remote add origin https://github.com/cheick224/exercice-.git

---
+++ exo2.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+n = int(input("entrez un nombre "))
+if (n % 2)== 0:
+   print("{0} est paire ".format(n))
+else:
+    print("{0} est impaire ".format(n))
+
---
+++ exo3.py
@@ -0,0 +1,5 @@
+#coding:utf-8
+for n in range (1,10):
+	print(" la table de multiplication de ", n," est:")
+	for i in range(1,10):
+		print(i, "x", n, "=",i*n)
